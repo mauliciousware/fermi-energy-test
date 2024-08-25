0b129719e664a489af1e9d8d3d3d2bb95cc6af0c
Amirali Azim <aazimita@uwaterloo.ca>
2024-08-25 08:38:31 -0400

    3-add-values-to-landing-page value prop updates

--- src/components/value-prop.tsx
+++ src/components/value-prop.tsx
@@ -1,46 +1,68 @@
-const valueprops = [
+import { 
+  FolderSync, 
+  SendToBack,
+  Gauge,
+  BrainCircuit 
+} from 'lucide-react';
+
+interface Feature {
+  title?: string;
+  desc: string | JSX.Element;
+}
+
+interface ValueProp {
+  name: string;
+  id: string;
+  description: string;
+  icon: JSX.Element;
+  features: Feature[];
+}
+
+const valueprops: ValueProp[] = [
+    {
+      name: "Transferability",
+      id: "end-to-end",
+      description: "Our documentation is available even before you start working with us so you can be sure that your future CTO has everything they need to continue growing your business.",
+      icon: <FolderSync/>,
+      features: [
+        {desc:<span>Access <strong>comprehensive documentation</strong> before our partnership begins</span>},
+        {desc: <span>Ensure a <strong>seamless transition</strong> for any future CTO</span>},
+        {desc: <span>Empower your team with all the necessary resources to continue <strong>business growth</strong></span>}
+      ],
+    },
+    {
+      name: "End-to-End",
+      id: "end-to-end",
+      description: "We help you with all stages from design, development, to deployment. We also have experience with sales and marketing and can give guidance on those areas from a technical perspective.",
+      icon: <SendToBack />,
+      features: [
+        {desc: <span>Manage every stage of building your business: <strong>design</strong>, <strong>development</strong>, and <strong>deployment</strong></span>},
+        {desc: <span>Provide technical guidance on industry standard <strong>marketing</strong> and <strong>analytics tools</strong></span>}
+      ],
+    },
     {
       name: "Speed",
       id: "speed",
       description: "Typical MVPs take us a month to build from start to finish and at maximum we expect to spend 2 months. We take out all the fluff and just focus on what matters.",
-      iconUrl:"/landing/speed.svg",
+      icon: <Gauge />,
       features: [
-        {title: "Rapid MVP Development", desc: "Typical MVPs take us a month to build from start to finish."},
-        {title: "Efficient Turnaround", desc: "At maximum we expect to spend 2 months."},
-        {title: "Lean Execusion", desc: "We take out all the fluff and just focus on what matters."}
+        {desc: <span>Deliver your project from <strong>start to finish</strong> within <strong>1-2 months</strong></span>},
+        {desc: <span>Maintain <strong>high quality</strong> while ensuring <strong>rapid execution</strong> and <strong>delivery</strong></span>},
+        {desc: <span>We use technology built for <strong>launching fast</strong> and <strong>scaling to millions.</strong></span>}
       ],
     },
     {
-        name: "End-to-End",
-        id: "end-to-end",
-        description: "We help you with all stages from design, development, to deployment. We also have experience with sales and marketing and can give guidance on those areas from a technical perspective.",
-        iconUrl:"/landing/end-to-end.png",
-        features: [
-          {title: "Comprehensive Development", desc: "We help you with all stages from design, development, to deployment."},
-          {title: "Technical Strategic Guidance", desc: "With our previous experience with sales and marketing, We can give guidance on those areas from a technical perspective."}
-        ],
-    },
-    {
-        name: "Scalability",
+        name: "Startup Experience",
         id: "scalability",
         description: "We use technology built for launching fast and scaling to millions. Our open sourced templated code is trusted by over 500 developers. Our founderʼs experience includes successfully scaling an app to over 150,000 active users.",
-        iconUrl:"/landing/scalable.png",
+        icon: <BrainCircuit />,
         features: [
-          {title: "Built for Growth", desc: "We use technology built for launching fast and scaling to millions."},
-          {title: "Proven Solution", desc: "Our open sourced templated code is trusted by over 500 developers."},
-          {title: "Proven Track Record", desc: "Our founderʼs experience includes successfully scaling an app to over 150,000 active users."}
+          {desc: <span>Founders with a track record of <strong>successful startup exits</strong></span>},
+          {desc: <span>Experience in scaling businesses to over <strong>200,000 active users</strong></span>},
+          {desc: <span>Deep understanding of what it takes to <strong>grow and sustain</strong> a business</span>}
         ],
     },
-    {
-        name: "Transferability",
-        id: "end-to-end",
-        description: "We understand that having an in house CTO is the best. When you do find a CTO, we have openly available documentation to make the transfer as seamless as possible.",
-        iconUrl:"/landing/transferability.png",
-        features: [
-          {title: "Smooth Transition", desc:"We understand that having an in house CTO is the best, so we facilitate a seamless transition when you hire one."},
-          {title: "Comprehensive Documentation", desc: "When you do find a CTO, we have openly available documentation to make the transfer as seamless as possible."}
-      ],
-    },
+
   ];
 
 export default function ValueProp() {
@@ -49,15 +71,15 @@ export default function ValueProp() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-4xl text-center">
             <h2 className="text-base font-semibold leading-7 text-primary">
-              Why Choose Us
+              Move your startup forward while looking for a perfect fit for your in house CTO
             </h2>
             <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-              At DevtoDollars
+              Why DevToDollars?
             </p>
           </div>
-          <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
+          {/* <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
             We provide you with the expertise needed to define your requirements and build a world-class, scalable solution using the latest technologies. This allows you to move forward with your business while we handle the technical development.
-          </p>
+          </p> */}
           <div className="flex flex-col lg:flex-row mx-auto max-w-md lg:max-w-none flex-wrap justify-between gap-16 lg:gap-5">
             {valueprops.map((tier) => (
                 <div
@@ -66,7 +88,7 @@ export default function ValueProp() {
                 style={{ flex: '1 1 22%' }}
                 >
                     <div className="flex items-center align-middle gap-x-4 ">
-                        <img src={tier.iconUrl} alt={`${tier.name} icon`} className="h-12 w-12" />
+                      {tier.icon}
                         <h3
                         id={tier.id}
                         className="text-lg font-semibold leading-8 text-white mb-0"
@@ -75,11 +97,10 @@ export default function ValueProp() {
                         </h3>
                   
                     </div>
-                    <ul className="mt-4 pl-0 text-md leading-6 text-gray-300 list-none list-inside">
+                    <ul className="mt-4 pl-5 text-md leading-6 text-gray-300 list-disc">
                       {tier.features.map((feature, index) => (
                           <li key={index} className="mb-4">
-                            <div className="font-bold">{feature.title}</div>
-                            <div className="text-sm">{feature.desc}</div>
+                            <div className="text-md">{feature.desc}</div>
                           </li>
                       ))}
                     </ul>
