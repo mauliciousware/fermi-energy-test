f723402dfd631b5df1a125a61050793fe31bd239
Amirali Azim <aazimita@uwaterloo.ca>
2024-10-30 21:27:47 -0400

    origin/3-add-values-to-landing-page added testimony and revamped values

--- src/components/hero-home.tsx
+++ src/components/hero-home.tsx
@@ -34,10 +34,10 @@ export default function HeroHome() {
       <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
         <h1 className="text-5xl font-bold tracking-tight text-white md:text-6xl lg:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-primary">
-          <span className="text-primary "> Looking for</span> 
+          <span className="text-primary "> Tech Co-founder</span> 
           <br /> {/* New line added here */}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-white tracking-wide">
-            a CTO?{" "}
+            as a Service{" "}
           </span>
         </h1>
           <p className="mt-6 text-lg leading-8 text-gray-300">
--- src/components/value-prop.tsx
+++ src/components/value-prop.tsx
@@ -25,9 +25,7 @@ const valueprops: ValueProp[] = [
       description: "Our documentation is available even before you start working with us so you can be sure that your future CTO has everything they need to continue growing your business.",
       icon: <FolderSync/>,
       features: [
-        {desc:<span>Access <strong>comprehensive documentation</strong> before our partnership begins</span>},
-        {desc: <span>Ensure a <strong>seamless transition</strong> for any future CTO</span>},
-        {desc: <span>Empower your team with all the necessary resources to continue <strong>business growth</strong></span>}
+        {desc:<span>Access <strong>comprehensive documentation</strong> before our partnership begins</span>}
       ],
     },
     {
@@ -37,7 +35,6 @@ const valueprops: ValueProp[] = [
       icon: <SendToBack />,
       features: [
         {desc: <span>Manage every stage of building your business: <strong>design</strong>, <strong>development</strong>, and <strong>deployment</strong></span>},
-        {desc: <span>Provide technical guidance on industry standard <strong>marketing</strong> and <strong>analytics tools</strong></span>}
       ],
     },
     {
@@ -47,8 +44,6 @@ const valueprops: ValueProp[] = [
       icon: <Gauge />,
       features: [
         {desc: <span>Deliver your project from <strong>start to finish</strong> within <strong>1-2 months</strong></span>},
-        {desc: <span>Maintain <strong>high quality</strong> while ensuring <strong>rapid execution</strong> and <strong>delivery</strong></span>},
-        {desc: <span>We use technology built for <strong>launching fast</strong> and <strong>scaling to millions.</strong></span>}
       ],
     },
     {
@@ -58,8 +53,6 @@ const valueprops: ValueProp[] = [
         icon: <BrainCircuit />,
         features: [
           {desc: <span>Founders with a track record of <strong>successful startup exits</strong></span>},
-          {desc: <span>Experience in scaling businesses to over <strong>200,000 active users</strong></span>},
-          {desc: <span>Deep understanding of what it takes to <strong>grow and sustain</strong> a business</span>}
         ],
     },
 
@@ -68,46 +61,46 @@ const valueprops: ValueProp[] = [
 export default function ValueProp() {
     return (
         <div className="pt-24 sm:pt-32 sm:pb-5 bg-[#343434]">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-4xl text-center">
-            <h2 className="text-base font-semibold leading-7 text-primary">
-              Move your startup forward while looking for a perfect fit for your in house CTO
-            </h2>
-            <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-              Why DevToDollars?
-            </p>
-          </div>
-          {/* <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
-            We provide you with the expertise needed to define your requirements and build a world-class, scalable solution using the latest technologies. This allows you to move forward with your business while we handle the technical development.
-          </p> */}
-          <div className="flex flex-col lg:flex-row mx-auto max-w-md lg:max-w-none flex-wrap justify-between gap-16 lg:gap-5">
-            {valueprops.map((tier) => (
-                <div
-                key={tier.id}
-                className="rounded-3xl p-4 xl:p-6 ring-1 ring-white/10"
-                style={{ flex: '1 1 22%' }}
-                >
-                    <div className="flex items-center align-middle gap-x-4 ">
-                      {tier.icon}
-                        <h3
-                        id={tier.id}
-                        className="text-lg font-semibold leading-8 text-white mb-0"
-                        >
-                        {tier.name}
-                        </h3>
-                  
-                    </div>
-                    <ul className="mt-4 pl-5 text-md leading-6 text-gray-300 list-disc">
-                      {tier.features.map((feature, index) => (
-                          <li key={index} className="mb-4">
-                            <div className="text-md">{feature.desc}</div>
-                          </li>
-                      ))}
-                    </ul>
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-none text-left">
+                    <h2 className="text-base font-semibold leading-7 text-yellow-400">
+                        Why work with us
+                    </h2>
+                    <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+                        Our Values at DevToDollars
+                    </p>
+                    <p className="mt-0 max-w-2xl text-lg leading-8 text-gray-300">
+                        Move your business forward while looking for a perfect fit for your in house CTO
+                    </p>
                 </div>
-            ))}
+                <div className="flex flex-col lg:flex-row mx-auto max-w-md lg:max-w-none flex-wrap justify-between gap-16 lg:gap-5">
+                    {valueprops.map((tier) => (
+                        <div
+                            key={tier.id}
+                            className="rounded-3xl w-full lg:w-[calc(25%-1.25rem)]"
+                        >
+                            <div className="flex flex-col items-start gap-y-4">
+                                <div className="text-5xl text-black p-4 bg-yellow-400 border-2 border-yellow-400 shadow-lg shadow-yellow-400/50 rounded-lg w-18 h-18 flex items-center justify-center">
+                                    {tier.icon}
+                                </div>
+                                <h3
+                                    id={tier.id}
+                                    className="text-lg font-bold leading-8 text-white mb-0"
+                                >
+                                    {tier.name}
+                                </h3>
+                            </div>
+                            <ul className="mt-4 text-md pl-0 text-gray-300 list-none">
+                                {tier.features.map((feature, index) => (
+                                    <li key={index} className="mb-4">
+                                        <div className="text-md">{feature.desc}</div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-      </div>
     )
 }
